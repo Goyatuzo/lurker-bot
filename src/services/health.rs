0a01b5cc328a9bf68df54755d1c2ef879b4a884c
Yuto Otaguro <yotagur@gmail.com>
2023-01-03 06:21:34 +0000

    Connect discord (#2)
    * Bot connects to discord
    
    * Remove redundant information

--- src/services/health.rs
+++ src/services/health.rs
@@ -3,9 +3,9 @@ use crate::models::health::{HealthInfo, HealthStatus};
 pub struct HealthService {}
 
 impl HealthService {
-	pub fn get_status() -> HealthInfo {
-		HealthInfo {
-			status: HealthStatus::OK
-		}
-	} 
+    pub fn get_status() -> HealthInfo {
+        HealthInfo {
+            status: HealthStatus::OK,
+        }
+    }
 }
